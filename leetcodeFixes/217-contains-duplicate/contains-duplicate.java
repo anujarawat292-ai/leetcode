8ceab31ef7cb714c1661a4c3338f32e1fe0ceeef
AnujaR <anujarawat292@gmail.com>
2026-08-25 21:55:45 +0530

    Time: 21 ms (35.12%) | Memory: 108.3 MB (28.86%) - LeetSync

---
+++ leetcodeFixes/217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        Set<Integer> st = new HashSet<>();
+        for(int i =0; i<n; i++){
+            if(st.contains(nums[i])){
+                return true;
+            }else{
+                st.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
